41d230a3ea9c5d2d1de86fa27efe28a1c3ced7e4
davidabutler92 <davidabutler92@yahoo.com>
2020-03-23 13:32:38 -0700

    add function liters to pints

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function convert(number1) {
+	return number2;
+  }
+ 
+var number1 = parseInt(prompt("Enter a number of liters to be converted to pints"))
+
+var number2 = (number1 * 2.11338);
+
+alert(number2)
